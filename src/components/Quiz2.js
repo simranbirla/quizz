a56026b1080036af6d42aaba158cfc421cefd219
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-09-01 14:24:21 +0530

    test done

--- src/components/Quiz2.js
+++ src/components/Quiz2.js
@@ -2,26 +2,49 @@ import React, { useState, useEffect } from "react";
 import questions from "./sampleQues";
 const Quiz2 = () => {
   const [num, setNum] = useState(0);
-
+  const [ans, setAns] = useState("");
   const renderQues = (num) => {
     return (
       <div>
         {questions[num].ques}
         {questions[num].options.map((opt) => {
-          return <p onClick={(e) => check(e)}>{opt}</p>;
+          return (
+            <p
+              onClick={() => check(opt)}
+              style={{
+                color: `${
+                  ans !== opt
+                    ? "black"
+                    : ans === questions[num].answer
+                    ? "green"
+                    : "red"
+                }`,
+              }}
+            >
+              {opt}
+            </p>
+          );
         })}
       </div>
     );
   };
 
-  const check = (e) => {
-    console.log(e.target.innerText);
-    if (e.target.innerText === questions[num].answer) {
+  const check = (opt) => {
+    setAns(opt);
+    /* if (opt === questions[num].answer) {
       console.log("right");
-      setNum(num + 1);
+      setAns(true);
+      setTimeout(() => {
+        setAns(false);
+        setNum(num + 1);
+      }, 1000);
     } else {
       console.log("wrong");
-    }
+    }*/
+    setTimeout(() => {
+      setAns("");
+      setNum(num + 1);
+    }, 1000);
   };
 
   return <div>THIS IS 2ND QUIZ{renderQues(num)}</div>;
