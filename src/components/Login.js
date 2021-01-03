f036e086b9f340acbb38d783a2f5281e0abffdcf
simranbirla <62006675+simranbirla@users.noreply.github.com>
2021-01-03 09:06:09 +0530

    login done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,21 @@
 import React from "react";
+import "../Style/Login.css";
 
 const Login = (props) => {
   return (
-    <div>
-      <input
-        placeholder="Enter your name"
-        onChange={(e) => props.setUser(e.target.value)}
+    <div className="login">
+      <img
+        src="https://as1.ftcdn.net/jpg/01/72/68/16/500_F_172681633_rLNC8wL3lMgFBjUG4n8fMLGH5gX8TYWB.jpg"
+        alt="quiz word"
       />
-      <button onClick={() => props.setSign(true)}>Login</button>
+      <form onSubmit={() => props.setSign(true)}>
+        <input
+          placeholder="Enter your name"
+          onChange={(e) => props.setUser(e.target.value)}
+          required
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 };
